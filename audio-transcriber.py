cc64a1f6384f20d53320a599723ef55ffd4b353a
Gabriel Sampaio <gsampaio@redhat.com>
2023-07-18 15:47:51 -0300

    diarize but no matching

--- audio-transcriber.py
+++ audio-transcriber.py
@@ -2,14 +2,11 @@ import whisperx
 import time
 import torch
 import os
+import re
 from pydub import AudioSegment
+from faster_whisper import WhisperModel
+from pyannote.audio import Pipeline
 
-device = ""
-"""Check CUDA availability."""
-if torch.cuda.is_available() == 1:
-    device = "cuda"
-else:
-    device = "cpu"
 
 def banner(text):
     """Display a message when the script is working in the background"""
@@ -33,6 +30,68 @@ def get_model():
     while not choice.isdigit() or int(choice) not in range(1, len(models)+1):
         choice = input("Invalid choice. Please enter the number of the model you wish to use: ")
     return models[int(choice)-1]
+
+def diarize_audio_segments(audio_file, hf_token):
+    
+    banner("Diarize audio")
+    start_time = time.time()
+    print(f"Start diarize time: {print_date(start_time)}")
+    
+    pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization', use_auth_token=hf_token)
+    diarize_segments = pipeline(audio_file)  
+
+    end_time = time.time()
+    print(f"End diarize time: {print_date(end_time)}")
+    elapsed_time = end_time - start_time
+    print("Total time taken to diarize the full text: ")
+    print_time(elapsed_time)
+
+    return diarize_segments
+
+def millisec(timeStr):
+  spl = timeStr.split(":")
+  s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
+  return s
+
+def attach_audio_segments(audio_file,diarize_segments):
+
+    banner("Attach audio")
+    start_time = time.time()
+    print(f"Start attach audio time: {print_date(start_time)}")
+    
+    spacermilli = 2000
+    sounds = AudioSegment.silent(duration=spacermilli)
+    audio_segment = AudioSegment.from_file(audio_file)  # Load the audio segment
+    segments = []
+
+    # print(diarize_segments)
+    # print("Type: ")
+    # print(type(diarize_segments)) 
+
+    for l in str(diarize_segments).splitlines():
+        print (l)
+        start, end =  tuple(re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=l))
+        start = int(millisec(start)) #milliseconds
+        end = int(millisec(end))  #milliseconds
+        
+        print (start)
+        print (end)
+
+        segments.append(len(sounds))
+        segment = audio_segment[start:end]  # Extract the desired segment
+        sounds = sounds.append(segment, crossfade=0)  # Append the segment to the sounds
+        # sounds = sounds.append(audio_file[start:end], crossfade=0)
+        # sounds = sounds.append(spacer, crossfade=0)
+
+    sounds.export("dz.wav", format="wav")
+
+    print(segments[:8])
+
+    end_time = time.time()
+    print(f"End attach audio time: {print_date(end_time)}")
+    elapsed_time = end_time - start_time
+    print("Total time taken to attach audio: ")
+    print_time(elapsed_time)
 
 def print_time(total_time):
     """Prints time in seconds or minutes"""
@@ -103,7 +162,7 @@ def transcribe_audio(audio_file, model):
 
     return result
     
-def format_result(result, audio_file):
+def format_result(result, audio_file, diarize_segments, device):
     banner("Format result")
     start_time = time.time()
     print(f"Start format time: {print_date(start_time)}")
@@ -114,6 +173,10 @@ def format_result(result, audio_file):
 
     # align whisper output
     result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file, device)
+    
+    # assign text
+    # result_assigned = whisperx.assign_word_speakers(diarize_segments, result_aligned)
+    # print(result_assigned["segments"])
 
     # Write transcription to file
     output_file = os.path.splitext(audio_file)[0] + '.txt'
@@ -131,8 +194,10 @@ def format_result(result, audio_file):
 
 def main():
     """Main function."""
-    audio_file = input("Please enter the path to the MP3 file: ")
-
+    # audio_file = input("Please enter the path to the MP3 file: ")
+    audio_file = "audio_segment.wav"
+    hf_token="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"
+    
     # Get the duration of the audio file
     audio = AudioSegment.from_file(audio_file)
     duration = audio.duration_seconds
@@ -140,15 +205,31 @@ def main():
     print_audio_info(audio_file)
     
     model_name = get_model()
-    model = whisperx.load_model(model_name, device=check_device())
+    device = check_device()
+    compute_type = "float32"
+    model = whisperx.load_model(model_name, device=device)
 
-    total_estimated_time, model = estimate_transcription_time(audio_file, model)
+    banner("Model information:")
+    print("Model Name: {}".format(model_name))
+    print("Device: {}".format(device))
+    print("Compute Type Name: {}".format(compute_type))
+
+    # Estimate
+    # total_estimated_time, model = estimate_transcription_time(audio_file, model)
 
     # Transcribe the full audio file
     result = transcribe_audio(audio_file, model)
-    
+    # print(result)
+
+    # Diarize audio
+    diarize_segments = diarize_audio_segments(audio_file, hf_token)
+    # print(diarize_segments)
+
+    # Attach Audio
+    attach_audio_segments(audio_file, diarize_segments)
+
     # Format the result
-    format_result(result, audio_file)
+    format_result(result, audio_file, diarize_segments, device)
 
 if __name__ == "__main__":
     main()
